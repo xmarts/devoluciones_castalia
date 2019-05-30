6bd3ee94f4c3860a771302de7b967eaf3d43e123
luis <angelchicles97@gmail.com>
2019-05-30 11:00:15 -0500

    menut en el inventario

--- modulo/devoluciones/__manifest__.py
+++ modulo/devoluciones/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale','purchase'],
+    'depends': ['base', 'sale','purchase','stock'],
 
     # always loaded
     'data': [
